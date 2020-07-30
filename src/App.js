8b87a34086e87690c31443eab0a86eb01025da76
Anthony Gelder <anthony.gelder@gmail.com>
2020-07-29 21:54:41 -0700

    components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, {Component} from 'react'
 import { Route, Link } from 'react-router-dom'
+import Header from './Header/Header'
 import './App.css'
 
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      
-    }
-  }
-
   render() {
     return (
       <div>
         <header>
-          <Route path='/' component={} />
+          <Route path='/' component={Header} />
         </header>
         <main className='App'>
           <Link to='/'>
